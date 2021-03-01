9915de40700c765e9da4c54c05b642b41ea139ce
ceejay15 <joie_pleaides@yahoo.com>
2021-03-01 09:47:57 +0800

    committing uncomitted files

--- src/components/game.js
+++ src/components/game.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import Textarea from './textarea';
+import { randomwords } from '../test_data/random_words.js';
 
 class Game extends Component{
     constructor(props) {
@@ -9,53 +10,56 @@ class Game extends Component{
         this.state = {
             errorMessage:"",
             paragraph:"",
-            length:0,            
+            length:0,   
+            level:0,
+            duration:0,
+            completed:false,
+            words:0,   
+            enabled: false      
         }
-    }   
+    } 
 
     componentDidMount(){
         axios.get("https://gist.githubusercontent.com/ceejay15/3d7033b3aba7675a51f4afa20eeceb8e/raw/be7fe5dde8ef48d3ea5a7ead3ca43a70b2dd9efc/random_words.json")
-        .then(respond=>{
-            console.log(respond?.data?.beginner?.paragraphs[2]?.words);
+        .then(respond=>{           
 
-            this.setState({                
-                length: respond?.data?.beginner?.paragraphs.length
-            })
-           
-           this.randomwords(respond?.data?.beginner?.paragraphs);
-           
+            let index = this.randomwords(respond?.data?.beginner?.paragraphs.length);
+
+            this.setState({
+                length : respond?.data?.beginner?.paragraphs.length,
+                level: 1,
+                duration:20,
+                paragraph: respond?.data?.beginner?.paragraphs[index]?.words,
+                words: respond?.data?.beginner?.paragraphs[index]?.words?.length
+            })           
         })
-        .catch(error=>{
-            //console.log(error);
+        .catch(error=>{           
             this.setState({errorMessage: "There is a problem connecting to API!"})
             console.log(this.state.errorMessage);
-            
-        })
+        })        
     }
 
-    randomwords(paragraph){
-        let len = this.state.length;
+    randomwords = (len) =>{
         let index = Math.floor(Math.random() * len);
-
-        console.log('length::',this.state.length, "index ", index, paragraph[index].words);
-        this.setState({paragraph:paragraph[index].words})
-        
+        return index;
     }
 
     render(){
-        return(<div className="container">
-                <h5 className="text-white">{this.state.errorMessage ? this.state.errorMessage : null}</h5>                
-                <div className="d-flex col-12">                    
-                    <div className="col-6">
-                       <div className="paragraph"><h4>{this.state.paragraph? this.state.paragraph:null}</h4></div>
-                        <Textarea name="player1" player="player1"/>
-                    </div>
-                    <div className="col-6">
-                      <div className="paragraph"><h4>{this.state.paragraph? this.state.paragraph:null}</h4></div>
-                       <Textarea name="player2" player="player2"/>
+        return(
+            <div className="container">
+                <h4 className="text-white">Level : {this.state.level = 1 ? "Beginner" : null}  </h4>               
+                <div className="p-1">  
+                  <h5 className="text-white">{this.state.errorMessage ? this.state.errorMessage : null}</h5>                           
+                    <div className="col-12">                       
+                        <Textarea values={this.state.duration !== 0 ? this.state : null}/>
                     </div>
                 </div>
-        </div>)
+        </div>
+        )
     }
+
+
+	
 }
+
 export default Game
--- src/components/textarea.js
+++ src/components/textarea.js
@@ -1,24 +1,108 @@
 import React, {Component} from 'react';
 
 class Textarea extends Component{
+   
     constructor(props) {
         super(props);
 
         this.state = {
             errorMessage:"",
-            paragraph:""            
+            paragraph:"", 
+            length:0,   
+            level:0,
+            duration:0,
+            completed:false,
+            words:0,   
+            enabled: false,
+            value : "",
+            charIndex:0,
+            mistakes:0,
+            accuracy:0,
+             
         }
-    }   
 
-    getText = e =>{
-        console.log(e.target.value)
+        console.log('props: ',props);
+    }  
+
+    componentWillReceiveProps(nextProps) {
+		this.props = nextProps;       
+	}
+    
+      
+    getText = e =>{  
+       
+        let words = this.state.paragraph;
+        let index = this.state.charIndex;
+        let wordsLen = this.state.words;//no of words
+        let accuracy = this.state.accuracy;
+        let mistakes = this.state.mistakes; 
+        let value = this.state.value;
+        let length = this.state.length;
+
+        
     }
 
-    render(){
-        return(<div className="container">
-                <div className="paragraph"></div>
-                <div className="d-flex col-12">
-                    <textarea className="player_text" cols="50" rows="10" onChange={this.getText.bind(this)}></textarea>
+    getValue = () =>{
+
+    }
+
+    start = () =>{
+        console.log("START!",this.props)
+
+        this.props.values.enabled = true;
+
+        this.setState({
+            enabled : this.props.values.enabled,
+            words: this.props.values.words,
+            duration: this.props.values.duration,
+            length: this.props.values.length,
+            paragraph : this.props.values.paragraph
+        })
+
+        let duration = this.props.values.duration;       
+
+        //this.startTimer(duration);
+    }
+
+    startTimer = (duration) =>{
+        const now = Date.now()
+		const seconds = now + duration * 1000
+		
+        let interval = setInterval(() => {
+			const secondLeft = Math.round((seconds - Date.now()) / 1000)
+            
+            console.log(secondLeft);
+              this.setState({duration: secondLeft})
+              if(secondLeft == 0){
+                  clearInterval(interval);
+              }
+		}, 1000)        
+    }
+   
+    
+    render(){   
+        let value = this.state.value;
+        return(<div className="container">                                  
+                <h5 className="text-white">Seconds left: {this.state.duration}</h5>                    
+                 <button type="button" className="btn btn-info" onClick={this.start}>Start!</button>  
+                 <div className="p-5">
+                    { this.state.paragraph ?  <div className="paragraph"><h4>{this.state.paragraph? this.state.paragraph:null}</h4></div> :'' }
+                 </div>                       
+                <div className="col-12">
+                    {
+                        this.state.enabled ? 
+                    
+                    <textarea type="text" tab="0" className="form-control text-white" onKeyDown={this.getText.bind(this)}></textarea>
+                    : ""
+                    }
+                    {
+                        this.state.value != "" ?
+                    
+                        <div className="card">
+                            <div className="card-body" tabIndex="0">{this.state.value ? this.state.value : null}</div>
+                        </div>
+                    : ""
+                    }
                 </div>
         </div>)
     }
